c3ed85e85aee92afc7a292a2aacdb24abe3e31c2
Juan Sgarbi <juan.sgarbi1@hotmail.com>
2023-03-21 10:59:06 -0300

    feat: add initial server configs

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import "express-async-errors";
+import "reflect-metadata";
+import express, { Application } from "express";
+import { errorHandler } from "./errors";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.use(errorHandler);
+
+export default app;
